4b629f71e0c9631f1df31f985c954cce6e49b1ea
Hyung Kim <hjkmines@gmail.com>
2020-10-01 07:58:54 -0600

    correct production route

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(require('./routes/user'));
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
 
